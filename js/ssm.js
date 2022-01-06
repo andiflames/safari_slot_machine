c108482d2fb57f584938998ab09a3e75a6fee8bd
andiflames <andiflames@gmail.com>
2022-01-06 17:07:50 -0500

    Completed cash out modal in html and js

--- js/ssm.js
+++ js/ssm.js
@@ -1,8 +1,8 @@
 // Image URLs 
-const giraffeImageUrl = "./images/baby-giraffe.png";
-const elephantImageUrl = "./images/baby-elephant.png";
-const monkeyImageUrl = "./images/monkey.png";
-const lionImageUrl = "./images/baby-lion.png";
+const giraffeImageUrl = './images/baby-giraffe.png';
+const elephantImageUrl = './images/baby-elephant.png';
+const monkeyImageUrl = './images/monkey.png';
+const lionImageUrl = './images/baby-lion.png';
 const imageOptions = {
     'giraffe': giraffeImageUrl,
     'elephant': elephantImageUrl,
@@ -14,13 +14,14 @@ const imageOptions = {
 const spinner1 = document.getElementById('spinner1');
 const spinner2 = document.getElementById('spinner2');
 const spinner3 = document.getElementById('spinner3');
-const spinBtn = document.querySelector("#spinBtn");
-let activeBet = document.getElementsByClassName("active-bet")[0];
-const creditsDisplay = document.getElementById("credits");
+const spinBtn = document.querySelector('#spinBtn');
+let activeBet = document.getElementsByClassName('active-bet')[0];
+const creditsDisplay = document.getElementById('credits');
 const resultsArea = document.getElementById('results');
 const gameOver = document.getElementById('gameOver');
 const payoutSchedule = document.getElementById('payoutSchedule');
 const cashOut = document.getElementById('cashOut');
+const cashOutModal = document.getElementById('cashOutModal');
 
 // Credits
 let currentCredits = 0;
@@ -29,7 +30,7 @@ let currentCredits = 0;
 let gameEnded = false;
 
 // Event Listeners  
-spinBtn.addEventListener("click", () => {
+spinBtn.addEventListener('click', () => {
     if (gameEnded) {
         initializeGame();
         return;
@@ -45,9 +46,13 @@ document.querySelectorAll('.bets').forEach(item => {
     });
 });
 
+//Once the Cash Out Div is clicked the page will load a section that states a message and provide your winnings.
+//After that update credits to 0 and uses the Game over check function so the player can restart the game.
 cashOut.addEventListener('click', () => {
-    alert(`You have won ${currentCredits} credits!! Thanks for playing!`)
-    initializeGame();
+    cashOutModal.style.display = 'block'
+    cashOutModal.textContent = `You have won ${currentCredits} credits!! Thanks for playing!`
+    updateCredits(0)
+    gameOverCheck()
 })
 
 // Helper Function
@@ -69,10 +74,10 @@ function arrayEquals(a, b) {
 
 function gameOverCheck() {
     if (currentCredits < 1) {
-        payoutSchedule.style.display = "none";
-        cashOut.style.display = "none";
-        gameOver.style.display = "block";
-        spinBtn.innerHTML = "Restart"
+        payoutSchedule.style.display = 'none';
+        cashOut.style.display = 'none';
+        gameOver.style.display = 'block';
+        spinBtn.innerHTML = 'Restart';
         gameEnded = true;
     }
 }
@@ -100,6 +105,7 @@ function calculateScore(results) {
     return 0;
 }
 
+// A function that updates the results area with the winnings won or credits lost in text string concatenation.
 function updateResultsArea(winnings) {
     if (winnings > 0) {
         resultsArea.textContent = `You've won ${winnings} credit(s)!`
@@ -133,16 +139,17 @@ function executeSpin() {
 }
 
 function setAsActiveBet(betOption) {
-    activeBet.classList.remove("active-bet");
-    betOption.classList.add("active-bet");
+    activeBet.classList.remove('active-bet');
+    betOption.classList.add('active-bet');
     activeBet = betOption;
 }
 
 function initializeGame(){
-    payoutSchedule.style.display = "block";
-    cashOut.style.display = "block";
-    gameOver.style.display = "none";
-    spinBtn.innerHTML = "Spin"
+    cashOutModal.style.display = 'none';
+    payoutSchedule.style.display = 'block';
+    cashOut.style.display = 'block';
+    gameOver.style.display = 'none';
+    spinBtn.innerHTML = 'Spin';
     gameEnded = false;
     updateCredits(50);
     changeSpinnerBackground(spinner1, giraffeImageUrl);
